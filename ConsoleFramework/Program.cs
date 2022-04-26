62404c307345634ad0d5ab7bd5a1d051367b03ad
Vladislav Kovalchuk <leshey95@gmail.com>
2022-04-26 19:09:07 +0300

    Minor improvements and new problems.

--- ConsoleFramework/Program.cs
+++ ConsoleFramework/Program.cs
@@ -4,20 +4,31 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Enter example in a + b format and then press Enter");
-        int firstNum = FirstNumInput();
-        char op = OperatorInput();
-        int secondNum = SecondNumInput();
-        PrintResult(firstNum, secondNum, op);    
-
+        Console.WriteLine("Enter example in a + b format and then press Enter\n" +
+            "Press E before or after any operation to Exit");
+        RunCalculator();
     }
 
-    public static void Backspace()
+    public static void RunCalculator() 
     {
-        Console.Write("\b \b");
+        while(true) 
+        {
+        ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
+            if (consoleKeyInfo.Key == ConsoleKey.E) 
+            {
+                long firstNum = GetNumInput();
+                char op = OperatorInput();
+                long secondNum = GetNumInput();
+                PrintResult(firstNum, secondNum, op);
+            }
+            else 
+            {
+                break;
+            }
+        }
     }
 
-    public static int FirstNumInput() 
+    public static long GetNumInput() 
     {
         StringBuilder stringBuilder = new StringBuilder();
         while (true)
@@ -28,51 +39,30 @@ public class Program
                 stringBuilder.Append(consoleKeyInfo.KeyChar);
                 Console.Write(consoleKeyInfo.KeyChar);
             }
-            else if (consoleKeyInfo.Key == ConsoleKey.Spacebar && stringBuilder.Length > 0) 
+            else if ((consoleKeyInfo.Key == ConsoleKey.Spacebar || consoleKeyInfo.Key == ConsoleKey.Enter) && stringBuilder.Length > 0) 
             {
-                Console.Write(consoleKeyInfo.KeyChar);
-                return Convert.ToInt32(stringBuilder.ToString());
+                if (consoleKeyInfo.Key == ConsoleKey.Enter) 
+                {
+                    Console.Write((char)ConsoleKey.Spacebar);
+                }
+                else 
+                {
+                    Console.Write(consoleKeyInfo.KeyChar);
+                }
+                return Convert.ToInt64(stringBuilder.ToString());
             }
-            else if (consoleKeyInfo.Key == ConsoleKey.Backspace)
+            else if (consoleKeyInfo.Key == ConsoleKey.Backspace && stringBuilder.Length > 0)
             {
-                Backspace();
+                Backspace(stringBuilder.Length);
                 stringBuilder.Length--;
-            }
-            
-        }
-    }
-    public static int SecondNumInput()
-    {
-        StringBuilder stringBuilder = new StringBuilder();
-        while (true)
-        {
-            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
-            if (char.IsDigit(consoleKeyInfo.KeyChar))
-            {
-                stringBuilder.Append(consoleKeyInfo.KeyChar);
-                Console.Write(consoleKeyInfo.KeyChar);
-            }
-            else if (consoleKeyInfo.Key == ConsoleKey.Spacebar || consoleKeyInfo.Key == ConsoleKey.Enter && stringBuilder.Length > 0)
-            {
-                return Convert.ToInt32(stringBuilder.ToString());
-            }
-            else if (consoleKeyInfo.Key == ConsoleKey.Backspace)
-            {
-                Backspace();
-                stringBuilder.Length--;
-                
             }
         }
     }
-
+  
     public static char OperatorInput() 
     {
         StringBuilder stringBuilder = new StringBuilder();
-        List<char> opList = new List<char>();
-        opList.Add('+');
-        opList.Add('-');
-        opList.Add('/');
-        opList.Add('*');
+        List<char> opList = new List<char>() {'+', '-', '/', '*'};
         
         while (true) 
         {
@@ -89,35 +79,45 @@ public class Program
             }
             else if (consoleKeyInfo.Key == ConsoleKey.Backspace)
             {
-                Backspace();
+                Backspace(stringBuilder.Length);
                 stringBuilder.Clear();
             }
         }
     }
 
-    public static int ExecuteMath(int num1, int num2, char op) 
+    public static long ExecuteMath(long num1, long num2, char op) 
     {
         switch (op) 
         {
             case '+': return num1 + num2;
             case '-': return num1 - num2;
             case '*': return num1 * num2;
-            case '/': 
-                if (num2 != 0) 
-                    return num1 / num2;
-                else 
-                    Console.WriteLine("\nYou cannot divide by zero!");
-                    return 0;
-            default: return 0;
+            case '/':
+                try 
+                {    
+                if (num2 != 0)
+                        return num1 / num2;
+                    else
+                        throw new DivideByZeroException("You cannot divide by zero!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw;
+                }
+                default: return 0;
         }       
     }
 
-    public static void PrintResult(int num1, int num2, char op) 
+    public static void PrintResult(long num1, long num2, char op) 
     {
-        int result = ExecuteMath(num1, num2, op);
-        if (op != '/' && num2 != 0)
-        {
-            Console.WriteLine($"\nResult is: {result}");
-        }
+        long result = ExecuteMath(num1, num2, op);
+        Console.WriteLine($"= {result}");
+    }
+
+    public static void Backspace(int strLength)
+    {
+        if (strLength > 0)
+            Console.Write("\b \b");
     }
 }
